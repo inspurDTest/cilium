dd781737bbc87161f6c4132eac1c28208037430f
pengbinbin1 <pengbiny@163.com>
2024-07-18 16:36:08 +0800

    fix

--- plugins/cilium-cni/chaining/generic-device/generic-device.go
+++ plugins/cilium-cni/chaining/generic-device/generic-device.go
@@ -139,14 +139,11 @@ func (f *GenericDeviceChainer) Add(ctx context.Context, pluginCtx chainingapi.Pl
 	}); err != nil {
 		return
 	}
-	/*if deviceIP == "" {
-		deviceIP = n.Type
-	}*/
 	switch {
 	case deviceMac == "":
 		err = errors.New("unable to determine device MAC address")
 		return
-	case deviceIP == "" && deviceIPv6 == "" && 1 != 1:
+	case deviceIP == "" && deviceIPv6 == "":
 		err = errors.New("unable to determine device IP address")
 		return
 	}
